019429ba9c689ab4dceb6c715e7ef464c433ab14
Giulia Real <giuliaguerreiro9@gmail.com>
2024-11-04 10:48:08 -0300

    Solution implemented for ServiceNow Instance

--- botSnow/bot.py
+++ botSnow/bot.py
@@ -21,56 +21,152 @@ Please refer to the documentation for more information at
 https://documentation.botcity.dev/tutorials/python-automations/web/
 """
 
-# Import for the Desktop Bot
-from botcity.core import DesktopBot
+import time
+# Import for the Web Bot
+from botcity.web import WebBot, Browser, By
 
 # Import for integration with BotCity Maestro SDK
 from botcity.maestro import *
-
+from pyshadow.main import Shadow
 # Disable errors if we are not connected to Maestro
+
+from selenium import webdriver
+from pyshadow.main import Shadow
+from datetime import datetime
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
+
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def main():
     # Runner passes the server url, the id of the task being executed,
     # the access token and the parameters that this task receives (when applicable).
     maestro = BotMaestroSDK.from_sys_args()
+
     ## Fetch the BotExecution with details from the task, including parameters
     execution = maestro.get_execution()
-
     print(f"Task ID is: {execution.task_id}")
     print(f"Task Parameters are: {execution.parameters}")
+    print("Starting execution...")
 
-    bot = DesktopBot()
-    # bot.browse("http://www.botcity.dev")
+    # Set driver configuration
+    driver_path = ChromeDriverManager().install()
+    driver = webdriver.Chrome(driver_path)
+    shadow = Shadow(driver)
 
-    # Implement here your logic...
-    bot.execute(r'C:\Program Files\Fakturama2\Fakturama.exe')
-    bot.wait(8000) # todo check/activate window
-    
-    # Opens Products Tab
-    if not bot.find("productsTab", matching=0.97, waiting_time=5000):
-        not_found("productsTab")
-    bot.click()
+    # Open page and insert credentials
+    shadow.driver.get("https://dev258555.service-now.com/now/nav/ui/classic/params/target/u_tech_products_list.do")
+    time.sleep(5)
 
+    name_input = driver.find_element(By.ID, "user_name")
+    name_input.send_keys("admin")
+    pwd_input = driver.find_element(By.ID, "user_password")
+    pwd_input.send_keys("iYy=FO3x1dN=")
+    time.sleep(2)
+    login_btn =driver.find_element(By.ID, "sysverb_login")
+    login_btn.click()
+    time.sleep(5)
 
-    if not bot.find("itemNumber", matching=0.97, waiting_time=5000):
-        not_found("itemNumber")
-    bot.click_relative(110, 6)
+    # Handle Shadow DOM elements and Iframe
+    frame = shadow.find_element_by_xpath('//*[@id="gsft_main"]')
+    shadow.chrome_driver.switch_to_frame(frame)
 
-    bot.paste("")
-    
+    # Getting table elements
+    table = driver.find_element(By.XPATH,'//*[@id="u_tech_products_table"]/tbody')
+    rows = table.find_elements(By.TAG_NAME, "tr")
+    for item in rows:
+        try:
+            fields = item.find_elements(By.TAG_NAME, "td")
+            name = fields[2].get_property("innerText")
+            category = fields[3].get_property("innerText")    
+            cost_price = fields[4].get_property("innerText")
+            gtin = fields[5].get_property("innerText")
+            id = fields[6].get_property("innerText")
+            price = fields[7].get_property("innerText")
+            quantity = fields[8].get_property("innerText")
+            supplier_code = fields[9].get_property("innerText")
 
-    print("Teste")
-    # Uncomment to mark this task as finished on BotMaestro
-    # maestro.finish_task(
-    #     task_id=execution.task_id,
-    #     status=AutomationTaskFinishStatus.SUCCESS,
-    #     message="Task Finished OK."
-    # )
+            print("Insert log entry for product... ")
+            maestro.new_log_entry(
+                activity_label="products-register",
+                values={
+                    "id": id,
+                    "product_name": name,
+                    "status": "REGISTERED"
+                }
+            )
 
+            new_item = DataPoolEntry(
+            values={
+                "name": name,
+                "entry-time": datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+                }
+            )
 
-def not_found(label):
-    print(f"Element not found: {label}")
+            # Getting the Datapool reference
+            datapool = maestro.get_datapool(label="tech-store-dw")
+
+            # Adding a new item
+            datapool.create_entry(new_item)
+
+            print("Sending products info for Datapool")
+            new_item = DataPoolEntry(
+                values={
+                    "name": name,
+                    "price": price,
+                    "amount": quantity,
+                    "cost_price": cost_price,
+                    "gtin": gtin,
+                    "supplier_code": supplier_code,
+                    "id": id,
+                    "Category": category
+                }
+            )
+
+            # Getting the Datapool reference
+            datapool = maestro.get_datapool(label="tech-store")
+            # Adding a new item
+            datapool.create_entry(new_item)
+
+        except Exception as error:
+            context =  "Could not registem product %s because an error occured " % name
+            maestro.alert(
+            task_id=execution.task_id,
+            title="Warning Alert",
+            message="Could not register product.",
+            alert_type=AlertType.WARN
+            )
+
+            # Registering error on maestro
+            maestro.error(task_id=execution.task_id, exception=context)
+            pass
+
+        print("Sending alerts of all products registered on maestro")
+        maestro.alert(
+            task_id=execution.task_id,
+            title="Info Alert",
+            message="All products Created successfully",
+            alert_type=AlertType.INFO
+            )
+            
+
+    # Flagging execution as finished
+    print("Finishing execution")
+
+    # List of emails, if not using, pass an empty list
+    emails = ["giulia.real@botcity.dev"]
+    # List of usernames, if not using, pass an empty list
+    users = []
+
+    subject = "Automation Finished"
+    body = "Hello, your automation was executed successfully"
+
+    maestro.message(emails, users, subject, body, MessageType.TEXT)
+    driver.close()
+    maestro.finish_task(
+        task_id=execution.task_id,
+        status=AutomationTaskFinishStatus.SUCCESS,
+        message="Automation was executed successfully")
 
 
 if __name__ == '__main__':
