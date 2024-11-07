79b60f44a784c1d453545b21062371fef6175c2f
Giulia Real <84285314+RealGiulia@users.noreply.github.com>
2024-11-07 17:10:55 -0300

    Delete botCotacao directory

--- botCotacao/build.ps1
+++
@@ -1,3 +0,0 @@
-$exclude = @("venv", "botCotacao.zip")
-$files = Get-ChildItem -Path . -Exclude $exclude
-Compress-Archive -Path $files -DestinationPath "botCotacao.zip" -Force
--- botCotacao/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zip -r "botCotacao.zip" * -x "botCotacao.zip"
